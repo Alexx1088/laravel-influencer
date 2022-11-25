2ec3584b4b861ea32d0459b7f893421fbaec7e85
Alexx108 <alexx1088@gmail.com>
2022-11-25 13:59:02 +0600

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+     //$user = User::factory(20)->make();
+    //    dd($user);*/
+
+
+    $this->call(UserSeeder::class);
+  //  User::factory(20)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
