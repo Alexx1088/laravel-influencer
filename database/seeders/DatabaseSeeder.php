6517c0482825e16ae6a9dc0ede81e58af4c3331c
Alexx1088 <alexx1088@gmail.com>
2022-12-28 21:56:43 +0600

    revert "first commit"
    This reverts commit 0accb8b34b5220f97a55e9e0daeb5ca98fc68cfc.
    
    revert

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,11 +17,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RoleSeeder::class,
-            PermissionSeeder::class,
-            RolePermissionSeeder::class,
             UserSeeder::class,
             ProductSeeder::class,
-            OrderSeeder::class
         ]);
     }
 }
