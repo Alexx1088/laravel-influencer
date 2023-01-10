f7ffca0657c90a497c9932a6096c58c9dec67026
Alexx108 <alexx1088@gmail.com>
2023-01-10 12:46:52 +0600

    first commit

---
+++ database/migrations/2023_01_09_131838_changing_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangingToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('name');
+            $table->dropColumn('email_verified_at');
+            $table->dropRememberToken();
+
+            $table->string('first_name');
+            $table->string('last_name');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
