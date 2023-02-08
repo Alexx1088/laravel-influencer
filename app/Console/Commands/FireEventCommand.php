22ba12541ed6f52a6d4f99ad163654b7f7985141
Alexx1088 <alexx1088@gmail.com>
2023-02-08 19:42:57 +0600

    add FireEventCommand

---
+++ app/Console/Commands/FireEventCommand.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\AdminAdded;
+use Illuminate\Console\Command;
+
+class FireEventCommand extends Command
+{
+       protected $signature = 'fire';
+
+
+    public function handle()
+    {
+        AdminAdded::dispatch();
+    }
+}
--- config/queue.php
+++ config/queue.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_CONNECTION', 'sync'),
+    'default' => env('QUEUE_CONNECTION', 'rabbitmq'),
 
     /*
     |--------------------------------------------------------------------------
@@ -70,8 +70,42 @@ return [
             'block_for' => null,
             'after_commit' => false,
         ],
+        'rabbitmq' => [
+            'driver' => 'rabbitmq',
+            'queue' => env('RABBITMQ_QUEUE', 'default'),
+            'connection' => PhpAmqpLib\Connection\AMQPLazyConnection::class,
 
+            'hosts' => [
+                [
+                    'host' => env('RABBITMQ_HOST', 'puffin-01.rmq2.cloudamqp.com'),
+                    'port' => env('RABBITMQ_PORT', 5672),
+                    'user' => env('RABBITMQ_USER', 'axivwufm'),
+                    'password' => env('RABBITMQ_PASSWORD', 'c53U7bC0W44Z8kbjl-at16esDDG25oVJ'),
+                    'vhost' => env('RABBITMQ_VHOST', 'axivwufm'),
+                ],
+            ],
+
+            'options' => [
+                'ssl_options' => [
+                    'cafile' => env('RABBITMQ_SSL_CAFILE', null),
+                    'local_cert' => env('RABBITMQ_SSL_LOCALCERT', null),
+                    'local_key' => env('RABBITMQ_SSL_LOCALKEY', null),
+                    'verify_peer' => env('RABBITMQ_SSL_VERIFY_PEER', true),
+                    'passphrase' => env('RABBITMQ_SSL_PASSPHRASE', null),
+                ],
+                'queue' => [
+                    'job' => VladimirYuldashev\LaravelQueueRabbitMQ\Queue\Jobs\RabbitMQJob::class,
+                ],
+            ],
+
+            /*
+             * Set to "horizon" if you wish to use Laravel Horizon.
+             */
+            'worker' => env('RABBITMQ_WORKER', 'default'),
+            'after_commit' => false,
+        ],
     ],
+
 
     /*
     |--------------------------------------------------------------------------
